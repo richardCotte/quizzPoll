de6af2c20a6fec627602703be7c3f7bafa2efb12
richardCotte <rcotte1582@gmail.com>
2022-02-28 15:06:43 +0100

    Back to finished poll

--- website/php/app/init.php
+++ website/php/app/init.php
@@ -2,12 +2,6 @@
 
 session_start();
 
-$db_username = 'root';
-$db_password = '';
+$_SESSION['user_id'] = 1;
 
-try {
-    $db = new PDO('mysql:host=localhost;dbname=pollwebsite', $db_username, $db_password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    $e->getMessage();
-}
+$db = new PDO('mysql:host=localhost;dbname=pollwebsite', 'root', '');
